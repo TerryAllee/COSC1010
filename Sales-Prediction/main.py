7d90db38b87db408f6563d2308f0236f378d53fd
Terry Allee <terryallee@westernwyoming.edu>
2024-09-02 17:07:02 -0600

    Update main.py

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Name Terry Allee
+# Date September 1st, 2024
 # Sales Prediction Programming Project
 # COSC 2409 DNT
 #
@@ -8,7 +8,10 @@
 # Variables to hold the sales total and the profit
 
 # Get the amount of projected sales.
-
+total_sales = float(input('Enter the projected sales:'))
 # Calculate the projected profit.
-
-# Print the projected profit.
+profit = total_sales * 0.23
+# print the projected profit.
+#print ('The profit is $', format(profit, ', .2f'))
+print ('The profit is $%.2f' % profit)
+#print (f'The profit is ${profit}')

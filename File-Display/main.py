df74a5519dfa320f4eec7e3b6addd051990347d2
Terry Allee <terryallee@westernwyoming.edu>
2024-10-19 18:06:34 -0600

    Add files via upload

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,19 @@
 #
-# Name
-# Date
+# Name: Terry Allee
+# Date: 10/19/2024
 # File Display Programming Project
-# COSC 2409 DNT
+# COSC 1010 
 #
 # Use comments liberally throughout the program. 
+
+
+# Open the file.
+myfile = open('/Users/terrygarcia/Library/CloudStorage/GoogleDrive-terrydallee@gmail.com/My Drive/COSC1010/File-Display/numbers.txt', 'r')
+
+# Read and display the file's contents.
+for line in myfile:
+    number = int(line)
+    print(number)
+
+# Close the file.
+myfile.close()

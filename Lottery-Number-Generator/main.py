9bb7f7138410516d6506d7bc395500374f93274f
Terry Allee <terryallee@westernwyoming.edu>
2024-10-28 18:49:00 -0600

    Add files via upload

--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,34 @@
 #
-# Name
-# Date
+# Name: Terry Allee 
+# Date: October 28, 2024
 # Lottery Number Generator Programming Project
-# COSC 2409 DNT
+# COSC 1010 
 #
 # Use comments liberally throughout the program. 
+
+import random
+
+# Use max number of digits
+MAX_DIGITS = 7 
+# Start with 0 for the number range
+START = 0
+# End with 9 on the random number range 
+END = 9
+
+# main function
+def main():
+    # Create the list
+    numbers = [0] * 7
+
+    # Populate the list with the random numbers
+    for index in range(MAX_DIGITS):
+        numbers[index] = random.randint(START, END)
+
+    # Display the random numbers.
+    print('Here are your lottery numbers:')
+    for index in range(MAX_DIGITS) :
+        print(numbers[index], end='')
+    print()
+
+# Call teh main function.
+main()
